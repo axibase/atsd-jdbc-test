3d13bf99442db449b052e147875cd049e110936b
Anna <snowswim@mail.ru>
2017-08-24 13:19:19 +0300

    travis.yml fixed

--- run_tests_in_container.sh
+++ run_tests_in_container.sh
@@ -6,9 +6,7 @@ TEST_OPTIONS=${2}
 if [ $(echo "${DOCKER_PORTS}"|sed 's/ -p/\n-p/g'|grep -c "p") -ne 2 ] && [ "${DOCKER_PORTS}" != "--publish-all" ];then
     echo "You must specify two ports at least or use --publish-all"
 else
-    timestamp="$(date +%s%N | cut -b1-13)"
-    CONTAINER_NAME=jdbc_test_${timestamp}
-    ./run_atsd_container.sh "${DOCKER_PORTS}" "${CONTAINER_NAME}"
+    CONTAINER_NAME=jdbc_test_$(date +%s%N | cut -b1-13)
 
     if [ -n "${TEST_OPTIONS}" ]; then
 	    echo "TEST_OPTIONS are ${TEST_OPTIONS}"

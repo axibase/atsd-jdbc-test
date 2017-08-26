a71da37635b706af6619fb6e68806ed12e9dcf0b
Anton Rybochkin <anton.rybochkin@axibase.com>
2017-08-26 19:18:07 +0300

    travis performance fix (#14)

--- run_atsd_container.sh
+++ run_atsd_container.sh
@@ -2,27 +2,42 @@
 
 SCRIPT=$(readlink -f $0)
 SCRIPTS_HOME="$(dirname ${SCRIPT})"
-DOCKERFILE="${SCRIPTS_HOME}/docker/"
-#HOST_NAME=$(cat /proc/sys/kernel/hostname)
-DOCKER_PORTS=${1}
-CONTAINER_NAME=${2}
+DOCKER_PORTS=$1
+CONTAINER_NAME=$2
+ATSD_LOGIN=$3
+ATSD_PASSWORD=$4
 
-docker build --no-cache=true -t atsd:${CONTAINER_NAME} ${DOCKERFILE}
+HOST=127.0.0.1
 
-docker run -d --name=${CONTAINER_NAME} ${DOCKER_PORTS} atsd:${CONTAINER_NAME}
+docker run -d --name=${CONTAINER_NAME} ${DOCKER_PORTS} -e axiname="$ATSD_LOGIN" -e axipass="$ATSD_PASSWORD" -e timezone="Europe/Berlin" axibase/atsd:api_test
 
 TCP_PORT=$(docker port ${CONTAINER_NAME} 8081| cut -d ":" -f2)
+HTTPS_PORT=$(docker port ${CONTAINER_NAME} 8443| cut -d ":" -f2)
+
 echo "TCP port is ${TCP_PORT}"
+echo "HTTPS port is ${HTTPS_PORT}"
 
 echo -n "Waiting for ATSD start"
-ans=""
-while true;
-	do
-		if [ -n "$ans" ]; then
-			echo -e "\nPing is ok"
-  		    break
-  		fi
-    sleep 3
-	ans=$(echo ping|nc 127.0.0.1 ${TCP_PORT})
-	echo -n "."
+while [[ $(curl --user $ATSD_LOGIN:$ATSD_PASSWORD --write-out %{http_code} --silent --insecure --output /dev/null https://$HOST:$HTTPS_PORT/version) != 200 ]];
+do
+    echo -n ".";
+    sleep 5;
 done
+echo "ATSD is ready"
+
+echo "==========================================="
+echo "Start data preparation at $(date +%Y-%m-%d\ %H-%M-%S)"
+echo "==========================================="
+
+echo "Inserting m_small with 100 records..."
+nc $HOST $TCP_PORT < $SCRIPTS_HOME/docker/m_small
+echo "m_small created"
+
+
+echo "Inserting m_large with 500000 records..."
+nc $HOST $TCP_PORT < $SCRIPTS_HOME/docker/m_large
+echo "m_large created"
+
+echo "==========================================="
+echo "Finished data preparation at $(date +%Y-%m-%d\ %H-%M-%S)"
+echo "==========================================="
--- run_tests_in_container.sh
+++ run_tests_in_container.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-DOCKER_PORTS=${1}
-TEST_OPTIONS=${2}
+DOCKER_PORTS=$1
+TEST_OPTIONS=$2
 
 if [ $(echo "${DOCKER_PORTS}"|sed 's/ -p/\n-p/g'|grep -c "p") -ne 2 ] && [ "${DOCKER_PORTS}" != "--publish-all" ];then
     echo "You must specify two ports at least or use --publish-all"

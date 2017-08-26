7b6052ebd6c5772ee478a6ea76a14716683adba6
Anton Rybochkin <anton.rybochkin@axibase.com>
2017-08-26 18:07:37 +0300

    travis fix

--- docker/ATSD_fill.sh
+++ docker/ATSD_fill.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-AXINAME=$1
-AXIPASS=$2
-HTTPS_PORT=$3
-TCP_PORT=$4
+HTTPS_PORT=$1
+TCP_PORT=$2
 SCRIPTS_HOME=$(dirname $0)
 HOST="127.0.0.1"
 
@@ -23,11 +21,6 @@ do
 	ping_ok=$(echo ping|nc $HOST $TCP_PORT)
 done
 logger "ATSD is ready"
-
-logger "Creating admin user=$AXINAME..."
-curl --data "userBean.username=$AXINAME&userBean.password=$AXIPASS&repeatPassword=$AXIPASS" https://$HOST:$HTTPS_PORT/login --insecure
-curl -u $AXINAME:$AXIPASS --data "options%5B0%5D.key=last.insert.write.period.seconds&options%5B0%5D.value=0&apply=Save" https://$HOST:$HTTPS_PORT/admin/serverproperties --insecure
-logger "User created"
 
 logger "Inserting m_small with 100 records..."
 nc $HOST $TCP_PORT < $SCRIPTS_HOME/m_small
--- run_atsd_container.sh
+++ run_atsd_container.sh
@@ -4,8 +4,10 @@ SCRIPT=$(readlink -f $0)
 SCRIPTS_HOME="$(dirname ${SCRIPT})"
 DOCKER_PORTS=$1
 CONTAINER_NAME=$2
+ATSD_LOGIN=$3
+ATSD_PASSWORD=$4
 
-docker run -d --name=${CONTAINER_NAME} ${DOCKER_PORTS} axibase/atsd
+docker run -d --name="atsd-jdbc-test" ${DOCKER_PORTS} -e axiname="$ATSD_LOGIN" -e axipass="$ATSD_PASSWORD" -e timezone="Europe/Berlin" axibase/atsd:api_test
 
 TCP_PORT=$(docker port ${CONTAINER_NAME} 8081| cut -d ":" -f2)
 HTTPS_PORT=$(docker port ${CONTAINER_NAME} 8443| cut -d ":" -f2)
@@ -13,4 +15,4 @@ HTTPS_PORT=$(docker port ${CONTAINER_NAME} 8443| cut -d ":" -f2)
 echo "TCP port is ${TCP_PORT}"
 echo "HTTPS port is ${HTTPS_PORT}"
 
-bash ${SCRIPTS_HOME}/docker/ATSD_fill.sh axibase axibase ${HTTPS_PORT} ${TCP_PORT}
+bash ${SCRIPTS_HOME}/docker/ATSD_fill.sh ${HTTPS_PORT} ${TCP_PORT}

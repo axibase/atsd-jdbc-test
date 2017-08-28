d63e2ab3123a66362ec464caa38f03f9ad6dd536
Anton Rybochkin <anton.rybochkin@axibase.com>
2017-08-28 19:10:45 +0300

    Added tests for getTags and setTags methods (#15)
    * added tests for getTags and setTags methods, fixed test utils
    
    * wait after series insert

--- src/test/java/com/axibase/tsd/driver/jdbc/data/ProtocolChecker.java
+++ src/test/java/com/axibase/tsd/driver/jdbc/data/ProtocolChecker.java
@@ -1,10 +1,10 @@
 package com.axibase.tsd.driver.jdbc.data;
 
 import com.axibase.tsd.driver.jdbc.content.ContentDescription;
-import com.axibase.tsd.driver.jdbc.content.StatementContext;
 import com.axibase.tsd.driver.jdbc.enums.Location;
 import com.axibase.tsd.driver.jdbc.ext.AtsdException;
 import com.axibase.tsd.driver.jdbc.protocol.SdkProtocolImpl;
+import com.axibase.tsd.driver.jdbc.rules.ExecuteWhenSysVariableSet;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.After;
 import org.junit.Before;
@@ -14,7 +14,7 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-import com.axibase.tsd.driver.jdbc.rules.ExecuteWhenSysVariableSet;
+import util.TestUtil;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -40,7 +40,7 @@ public class ProtocolChecker {
     public void setUp() throws Exception {
 		final ContentDescription contentDescription = new ContentDescription(
 				Location.SQL_ENDPOINT.getUrl(DEFAULT_CONNECTION_INFO), DEFAULT_CONNECTION_INFO,
-				SELECT_TVE_CLAUSE + SMALL_TABLE + SELECT_LIMIT_1000, new StatementContext());
+				SELECT_TVE_CLAUSE + SMALL_TABLE + SELECT_LIMIT_1000, TestUtil.createStatementContext());
 		this.protocol = PowerMockito.spy(new SdkProtocolImpl(contentDescription));
 	}
 
